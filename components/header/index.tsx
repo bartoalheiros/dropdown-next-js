3c2d5f366b3f7942dd6bda58fe77db2ea8279473
BartoAlheiros <alheirosb@gmail.com>
2024-04-01 16:41:34 -0300

    codigos do header

---
+++ components/header/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Header = () => {
+  return <div className='py-5 bg-gray-900 flex justify-center items-center'>
+            Header
+         </div>;
+  
+}
+
+export default Header
